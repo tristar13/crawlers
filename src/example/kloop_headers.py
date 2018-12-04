dc9cb176b1976ccfe1d5d8b1805bd09ac95a98c8
Rinatius <rinat@kloop.kg>
2018-12-04 16:34:13 +0600

    Add Kloop.kg headers crawler

---
+++ src/example/kloop_headers.py
@@ -0,0 +1,26 @@
+def extractdata(context, data):
+    # This stage comes after 'fetch' so the 'data' input contains an
+    # HTTPResponse object.
+    response = context.http.rehash(data)
+    url = response.url
+    page = response.html
+
+    # Parse the rest of the page to extract structured data.
+
+    header = _gettext(page.xpath('.//header/h1/text()'))
+
+    article_data = {
+        "url": response.url,
+        "header": header
+    }
+
+    if article_data["header"] is not None:
+        # If 'rule' is not set, it defaults to 'pass', which triggers the
+        # final 'store' stage.
+        context.emit(data=person_data)
+
+def _gettext(list):
+    if not list:
+        return list
+    else:
+        return list[0].strip()
