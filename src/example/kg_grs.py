7dda764d6aa925947ae26b0d0c0d897b4c8fd319
Sasha <tenlee1312@gmail.com>
2018-12-06 17:30:11 +0600

    Grs kyrgyzstan

---
+++ src/example/kg_grs.py
@@ -0,0 +1,27 @@
+def extractdata(context, data):
+    # This stage comes after 'fetch' so the 'data' input contains an
+    # HTTPResponse object.
+    response = context.http.rehash(data)
+    url = response.url
+    page = response.html
+
+    # Parse the rest of the page to extract structured data.
+
+    street = _gettext(page.xpath("//div[@class='list-street']/p"))
+    
+
+    org_data = {
+        "url": response.url,
+        "street": street
+    }
+    
+    
+	
+	
+    context.emit(data=org_data)
+
+def _gettext(list):
+    if not list:
+        return list
+    else:
+        return list[0].strip()
