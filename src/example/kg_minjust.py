52be72be21c7bcc1dd62d24cd8e60c595407c0eb
Sasha <tenlee1312@gmail.com>
2018-12-06 17:07:31 +0600

    Minjust new base

--- src/example/kg_minjust.py
+++ src/example/kg_minjust.py
@@ -77,7 +77,7 @@ def extractdata(context, data):
     }
     
     for key, value in org_data.items():
-      if value == "":
+      if value == " ":
            value ='----'
            newd = {}
            newd[key]=key
