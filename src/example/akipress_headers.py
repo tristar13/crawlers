1fbd7cd4b65c83736a93f2ee37b3fd9b6e52bb27
Sasha <tenlee1312@gmail.com>
2018-12-25 20:20:51 +0600

    Akipress parse

--- src/example/akipress_headers.py
+++ src/example/akipress_headers.py
@@ -8,23 +8,26 @@ def extractdata(context, data):
     print(response)
     # Parse the rest of the page to extract structured data.
 
-    header1 = _gettext(page.xpath('//div[@class]/h1/text()'))
+    if url = 'http://kg.akipress.org/':
+        header = _gettext(page.xpath('//body/div[5]//a'))
+    elif url = 'http://bilim.akipress.org/ru':
+        header = _gettext(page.xpath('//body/div[@class]/div[3]/div/div[@class][2]//div[@class]/a'))
+        
+    #header = _gettext(page.xpath('//div[@class]/h1/text()'))
     #header2 = _gettext(page.xpath('//div[@class='news_body']/h1[@class='newsheadline_title']/text()'))
     #header3 = _gettext(page.xpath('//div[@class='area_block']/div[@class='news_body']/text()'))
-    foto = _gettext(page.xpath('//div[@class]/img/@src'))
+    #foto = _gettext(page.xpath('//div[@class]/img/@src'))
     #foto2 = _gettext(page.xpath('//div[@class='area_block']/div[@class='news_body']/div[@class='photoin']/img/@src'))
 
 
     article_data = {
         "url": response.url,
-        "header1": header1,
-        #"header2": header2,
-        #"header3": header3,
-        "foto": foto
+        "header": header
+        #"foto": foto
         #"foto2":  foto2
     }
 
-    if article_data["header1"] is not None:
+    if article_data["header"] is not None:
         # If 'rule' is not set, it defaults to 'pass', which triggers the
         # final 'store' stage.
         context.emit(data=article_data)
